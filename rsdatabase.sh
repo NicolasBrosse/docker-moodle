1aed64db72b9b2083a30fc6de47d01eb47e31c4e
polo60 <pauljaillet60@gmail.com>
2022-02-17 16:54:05 +0100

    new database correction orthographe

--- rsdatabase.sh
+++ rsdatabase.sh
@@ -1,3 +1,3 @@
-echo indiquer la date de la database à restaurer '(/Y/m/d/H/M)' :
+echo indiquez la date de la database à restaurer '(/Y/m/d/H/M)' :
 read -p date: datevar
 docker exec -e MYSQL_DATABASE=moodle -e MYSQL_PWD=root docker-moodle-db-1 bash -c 'mysql -u root moodle < /opt/backups/docker-moodle-db-1-moodle-'$datevar'.sql'
