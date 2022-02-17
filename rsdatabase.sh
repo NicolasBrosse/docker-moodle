9e60fe7b66b7ab4268e3d3513d3a6fb8e7968d30
polo60 <pauljaillet60@gmail.com>
2022-02-17 16:51:27 +0100

    new database script

--- rsdatabase.sh
+++ rsdatabase.sh
@@ -1,1 +1,3 @@
-$docker exec -e MYSQL_DATABASE=moodle -e MYSQL_PWD=root docker-moodle-db-1 bash -c 'mysql -u root moodle < /opt/backups/docker-moodle-db-1-moodle-202202171202.sql'
+echo indiquer la date de la database à restaurer '(/Y/m/d/H/M)' :
+read -p date: datevar
+docker exec -e MYSQL_DATABASE=moodle -e MYSQL_PWD=root docker-moodle-db-1 bash -c 'mysql -u root moodle < /opt/backups/docker-moodle-db-1-moodle-'$datevar'.sql'
