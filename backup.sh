631c1d67d6d4095849c20c445e37fd7ff74f7dc2
polo60 <pauljaillet60@gmail.com>
2022-02-23 19:06:23 +0100

    chemin relatif backup.sh

--- backup.sh
+++ backup.sh
@@ -8,6 +8,8 @@
 
 DAYS=1
 BACKUPDIR=./backups
+BACKUPDIRV=./backups/volumes
+BACKUPDIRSQL=./backups/sql
 TIMESTAMP=$(date)
 
 # backup all mysql/mariadb containers
@@ -27,17 +29,17 @@ for i in $CONTAINER; do
 
     docker exec -e MYSQL_DATABASE=$MYSQL_DATABASE -e MYSQL_PWD=$MYSQL_PWD \
         $i mysqldump -u root --skip-comments $MYSQL_DATABASE \
-         | gzip > $BACKUPDIR/sql/$i-$MYSQL_DATABASE-$(date +"%Y%m%d%H%M").sql.gz 
+         | gzip > $BACKUPDIRSQL/$i-$MYSQL_DATABASE-$(date +"%Y%m%d%H%M").sql.gz 
 
-    OLD_BACKUPS=$(ls -1 $BACKUPDIR/sql/$i*.gz |wc -l)
+    OLD_BACKUPS=$(ls -1 $BACKUPDIRSQL/$i*.gz |wc -l)
     if [ $OLD_BACKUPS -gt $DAYS ]; then
-        find $BACKUPDIR -name "$i*.gz" -daystart -mtime +$DAYS -delete #<logs.txt
+    find $BACKUPDIR -name "$i*.gz" -daystart -mtime +$DAYS -delete #<logs.txt
     fi
-    tar -czf $BACKUPDIR/volumes/moodle_data-$(date +"%Y%m%d%H%M").tar.gz ./app/moodle_data
+    tar -czf $BACKUPDIRV/moodle_data-$(date +"%Y%m%d%H%M").tar.gz ./app/moodle_data
 
-    OLD_BACKUPS=$(ls -1 $BACKUPDIR/volumes/*.gz |wc -l)
+    OLD_BACKUPS=$(ls -1 $BACKUPDIRV/*.gz |wc -l)
     if [ $OLD_BACKUPS -gt $DAYS ]; then
-        find $BACKUPDIR -name "*.gz" -daystart -mtime +$DAYS -delete #<logs.txt
+    find $BACKUPDIR -name "*.gz" -daystart -mtime +$DAYS -delete #<logs.txt
     fi
     
 done
